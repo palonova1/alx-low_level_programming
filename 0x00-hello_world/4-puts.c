151e09d95f8e890da6edc9db40611e6c3e74c657
Winny Nyamohanga <riobawinny@gmail.com>
2022-06-30 15:41:42 -0700

    ghdh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-echo '#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
 /**
- *main - entry point
- *
- *Return: 0 after printing the function
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"*
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
-}'>4-puts.c
+}
