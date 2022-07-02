d2039a551c450130c7fe2a0ee48457d44f22125d
Winny Nyamohanga <riobawinny@gmail.com>
2022-07-01 19:57:05 -0700

    ghdhsh

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,20 +2,23 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - Prints the alphabet in lowercase
+*main - Task 3 prints the alphabet in lowercase, and then in uppercase
+*followed by a new line
 *
-*Return: 0 if success
+*Return: 0
 **/
 int main(void)
 {
-	char d = 'a';
+	int ch;
 
-	while (d <= 'z')
+	for (ch = 97; ch <= 122; ch++)
 	{
-	putchar(d);
-	d++;
+	putchar(ch);
 	}
-	putchar('\n');
+	for (ch = 65; ch <= 90; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10); /* this is the ASCII code for new line*/
 	return (0);
 }
-
