5d0664218bcfe4deb23f3e5a0d850c26e040dcdf
Winny Nyamohanga <riobawinny@gmail.com>
2022-07-05 08:17:54 -0700

    ghdhshh

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @z1: The first number to be computed
+  * @z2: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int z1, int z2)
+{
+	return (z1 + z2);
+}
