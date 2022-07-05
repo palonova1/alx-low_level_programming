8dcf189a08d97162578d3dfd24998194004cd354
Winny Nyamohanga <riobawinny@gmail.com>
2022-07-05 06:59:47 -0700

    gdjsjsj

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "main.h" 
+
 /**
-* main - Entry point
+* main - prints putchar
 *
-* Return: Always 0 (Success)
+* Return: Always 0
 */
 int main(void)
 {
-	char *sh = "main.h";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
