9e6b1db9cae1b2dcd260576069266618b9f98a37
Winny Nyamohanga <riobawinny@gmail.com>
2022-07-05 04:03:47 -0700

    gjdjj

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include "holberton.h" 
+#include "holberton.h"
+#include <main.h>
 /**
-*main - Entry point
+* main - Entry point
 *
-*Return: Always 0 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-char *sh = "Holberton";
+	char *sh = "Holberton";
 
-while (*sh)
-{
-	_putchar(*sh);
-	sh++;
-}
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
 
-return (0);
+	return (0);
 }
