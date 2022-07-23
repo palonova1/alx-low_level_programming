f398309dae03b331681655455f6547a65f98a19c
Winny Nyamohanga <riobawinny@gmail.com>
2022-07-22 22:28:00 -0700

    delete

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,3 +1,19 @@
 int _strlen(char *s)
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: string.
+ *
+ * Return: Length.
+ */
+int _strlen(char *s)
 {
+        int len = 0;
+
+        while (*s++)
+                len++;
+
+        return (len);
 }
+
