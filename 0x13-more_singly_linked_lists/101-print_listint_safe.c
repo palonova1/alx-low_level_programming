0a7ed6c9fc685027f494df48a7a4c6d34e0e2e39
winny Nyamohanga <107371330+palonova1@users.noreply.github.com>
2022-08-16 23:22:48 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,14 +4,12 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Entry Point
- * @head: head
- * Return: 0
- */
+  * print_listint_safe - define function
+  * @head:
+  * Return: what does it return?
+  */
 size_t print_listint_safe(const listint_t *head)
 {
-	if (head == NULL)
-		exit(98);
-
+	head++;
 	return (0);
 }
