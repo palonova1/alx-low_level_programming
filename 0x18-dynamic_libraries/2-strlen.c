2c36dca9cd84f0497be31c090dbf095272be405a
winny Nyamohanga <107371330+palonova1@users.noreply.github.com>
2022-10-08 00:43:50 +0300

    let check

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * _strlen - Returns the length of a string
+  * @str: String
+  *
+  * Return: length
+  */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
+}
